b15f6f658543639b91867c28cf14db9e43fb10d5
jess hust <jessicakhust@gmail.com>
2016-07-08 21:41:02 -0500

    Intial commit

---
+++ letter.js
@@ -0,0 +1,16 @@
+
+var Letter = function(chr) {
+	this.character = chr.toLowerCase();
+	this.appear = false;
+	this.letterRender = function() {
+		if(this.appear) {
+			return this.character;
+		} else if(this.character == " ") {
+			this.appear = true;
+			return " " + this.character + " ";
+		} else {
+			return "_ ";
+		}
+	}
+}
+exports.Letter = Letter;
